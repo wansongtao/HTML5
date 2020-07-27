938996104f60d5ebd51bb79d63ca541f4b51fa05
wansongtao <18101837209@163.com>
2020-07-27 22:15:06 +0800

    0727

--- chapter6/mightygumball.js
+++ chapter6/mightygumball.js
@@ -1,16 +1,17 @@
 window.onload = function () {
-    var url = "http://wickedlysmart.com/ifeelluckytoday";
+    var url = "file:///G:/software_programming/Web/Html5/chapter6/sales.json";
     var request = new XMLHttpRequest();
 
     request.open("GET", url);
-
-    if(request.status == 200){
-        displayLuck(request.responseText);
-    }
+    request.onload = function() {
+        if (request.status == 200){
+            updateSales(request.responseText);
+        }
+    };
     request.send(null);
 }
 
-function displayLuck(luck) {
-    var p = document.getElementById("luck");
-    p.innerHTML = "Today you are " + luck;
-}
+// function displayLuck(luck) {
+//     var p = document.getElementById("luck");
+//     p.innerHTML = "Today you are " + luck;
+// }
